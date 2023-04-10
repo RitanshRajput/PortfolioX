7d598b4c9e093f19b0c8533f51683d0fc074948b
Ritansh Rajput <it2042@global.org.in>
2023-04-10 21:39:16 +0530

    emoji search app done

--- src/Component/EmojiSearch.js
+++ src/Component/EmojiSearch.js
@@ -1,19 +1,39 @@
-import React, { useState } from 'react'; 
+import React, { useEffect, useState } from 'react'; 
 import EmojiData from '../Emoji.json' ;
 
 const EmojiSearch = () =>{
 
 const [search, setSearch] = useState('') ;
+const [data, setData] = useState([]) ;
+
+useEffect(() => {
+  const newData = EmojiData
+  .filter(emoji => emoji.title.toLowerCase()
+  .includes(search.toLowerCase())) ;
+
+  setData(newData) ;
+
+},[search]) ;
 
     return(
     <>
       <div id="emojiSearch">
         <center>
-            <input type="text" name='search' value={search} placeholder='Search Emoji..' onChange={()=> setSearch()}/>
+            <h2>Emoji Search</h2>
+            <input type="text" name='search' value={search} placeholder='Search Emoji..' onChange={(e)=> setSearch(e.target.value)}/>
         </center>
+
+      <div className="main-container">
+      { data.map(emoji =>
+        <div className='emojiContainer' key={emoji.title}>
+          <div className="emojis">
+            {emoji.symbol} {emoji.title}
+          </div>
+        </div>)} 
+      </div>
       </div>
     </>
     ) 
-}
+} ;
 
-export default EmojiSearch
+export default EmojiSearch ;
